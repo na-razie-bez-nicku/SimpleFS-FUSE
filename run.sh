7dba3011752e1afa452ef7dec081edc78c1aadc2
na-razie-bez-nicku <87973435+na-razie-bez-nicku@users.noreply.github.com>
2025-10-31 11:53:28 +0100

    Delete run.sh

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-sudo fusermount3 -u /mnt/simplefs
-sudo valgrind --tool=memcheck ./build/simplefs -o disk=/home/ubuntu/projects/SimpleFS-FUSE/disk2.img -o exec,allow_other,default_permissions /mnt/simplefs -f
